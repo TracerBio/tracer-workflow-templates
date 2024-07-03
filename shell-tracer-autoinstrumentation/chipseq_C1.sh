0340e8837865526df777e3c41bab8f68337c86a8
Tushar Warrier <tw1628@Tushars-MacBook-Pro.local>
2024-07-03 14:07:29 +0100

    Updated CHIPSEQ time-tracker for Case 1

--- shell-tracer-autoinstrumentation/chipseq_C1.sh
+++ shell-tracer-autoinstrumentation/chipseq_C1.sh
@@ -12,36 +12,36 @@ fastqc_duration=$((end_fastqc - start_fastqc));
 # Creating a human genome index using STAR
 echo "Creating a human genome index using STAR...";
 start_star_genome=$(date +%s%3N);
-STAR --runThreadN 4 --runMode genomeGenerate --genomeDir ./mouse_genome --genomeSAindexNbases 10 --genomeFastaFiles mouse.fa --sjdbGTFfile mm10.gtf --sjdbOverhang 99;
+STAR --runThreadN 4 --runMode genomeGenerate --genomeDir ./mouse_genome --genomeSAindexNbases 10 --genomeFastaFiles mus.fa --sjdbGTFfile hg19.refGene.gtf --sjdbOverhang 99;
 end_star_genome=$(date +%s%3N);
 star_genome_duration=$((end_star_genome - start_star_genome));
 
 # Align RNA-Seq reads to the genome using STAR
 echo "Aligning RNA-Seq reads to the genome using STAR...";
 start_star_align=$(date +%s%3N);
-STAR --runThreadN 4 --genomeDir ./mouse_genome --readFilesIn s1_1.fq s1_2.fq --outFileNamePrefix P1s1_mouse --outSAMtype BAM SortedByCoordinate;
+STAR --runThreadN 4 --genomeDir ./mouse_genome --readFilesIn s1_1.fq s1_2.fq --outFileNamePrefix P1s1 --outSAMtype BAM SortedByCoordinate;
 end_star_align=$(date +%s%3N);
 star_align_duration=$((end_star_align - start_star_align));
 
 # Sort and Index the output bam file
 echo "Sorting and indexing the BAM file...";
 start_samtools=$(date +%s%3N);
-samtools sort *.bam -@ 4 -o P1s1_mouse.sorted.bam;
-samtools index P1s1_mouse.sorted.bam;
+samtools sort *.bam -@ 4 -o P1s1.sorted.bam;
+samtools index P1s1.sorted.bam;
 end_samtools=$(date +%s%3N);
 samtools_duration=$((end_samtools - start_samtools));
 
 # Call Peaks for BAM file
 echo "Calling peaks for BAM file...";
 start_macs=$(date +%s%3N);
-macs3 callpeak -t P1s1_mouse.sorted.bam -f BAMPE -p 0.05 --outdir ./Peaks;
+macs3 callpeak -t P1s1.sorted.bam -f BAMPE -p 0.05 --outdir ./Peaks;
 end_macs=$(date +%s%3N);
 macs_duration=$((end_macs - start_macs));
 
 # Plot Coverage of Peaks
 echo "Plotting coverage of peaks...";
 start_plotCoverage=$(date +%s%3N);
-plotCoverage -b P1s1_mouse.sorted.bam -o coverage.pdf;
+plotCoverage -b P1s1.sorted.bam -o coverage.pdf;
 end_plotCoverage=$(date +%s%3N); 
 plotCoverage_duration=$((end_plotCoverage - start_plotCoverage));
 
