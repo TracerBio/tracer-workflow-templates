415d391ebcc471f48ed1e17cbd94ae686911d36f
Tushar Warrier <tw1628@Tushars-MBP.ultrahub>
2024-08-10 00:41:41 +0100

    Airflow config script

--- config/airflow.sh
+++ config/airflow.sh
@@ -4,4 +4,5 @@ cp ./shell-tracer-autoinstrumentation/dags/rnaseq_dag.py $AIRFLOW_HOME/dags/;
 airflow users create --username tracer --firstname Admin --lastname User --role Admin --email hello@tracer.bio;
 airflow db migrate;
 export AIRFLOW__CORE__LOAD_EXAMPLES=False;
-airflow scheduler &
+airflow scheduler &;
+airflow webserver --port 8080
