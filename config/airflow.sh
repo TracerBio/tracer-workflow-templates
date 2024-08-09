1df1d21db937fb132078d99a9fa82e9a27dbd5bc
Tushar Warrier <tw1628@Tushars-MBP.ultrahub>
2024-08-10 00:31:10 +0100

    Airflow config script

--- config/airflow.sh
+++ config/airflow.sh
@@ -2,4 +2,6 @@ export AIRFLOW_HOME=~/airflow;
 mkdir -p $AIRFLOW_HOME/dags;
 cp ./shell-tracer-autoinstrumentation/dags/rnaseq_dag.py $AIRFLOW_HOME/dags/;
 airflow users create --username tracer --firstname Admin --lastname User --role Admin --email hello@tracer.bio;
+airflow db migrate;
+export AIRFLOW__CORE__LOAD_EXAMPLES=False;
 airflow scheduler &
